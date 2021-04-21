4538b791aad87fa26f618803defe4467484eb664
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-04-21 14:33:05 -0400

    added register and login buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Logo from '../images/Logo.png'
-import '../css/Logo.css'
+import '../css/Homepage.css'
+import { Nav, Navbar, Button } from 'react-bootstrap';
 
 export default function Home() {
     return (
         <div>
-            <img className="logo"src={Logo} alt='Food-fight-logo.png'></img>
+            <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
+                <Navbar.Brand href="#home">Food Fight</Navbar.Brand>
+                <Nav>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link>
+                </Nav>
+            </Navbar>
+            <img className="logo" src={Logo} alt='Food-fight-logo.png'></img>
+            <h2>Welcome to Food Fight!</h2>
+            <p>oh boy im about to crumb</p>
+            <Button className="registerloginButton" >Register</Button>
+            <Button className="registerloginButton">Login</Button>
         </div>
     )
 }
