3f787c3e38454224a48e2b4f558a139688329186
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-04-26 14:21:59 -0400

    added drag and drop

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,7 +1,7 @@
 import { Button, Card, Row } from "react-bootstrap"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import { useSelector, useStore } from "react-redux";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import "../css/team.css"
@@ -9,6 +9,8 @@ import "../css/team.css"
 export default function Team() {
   const [team, setTeam] = useState([])
   const [teamUsers, setTeamUsers] = useState([])
+  const [teamRestaurants, setTeamRestaurants] = useState([])
+  const [fightRestaurants, setFightRestaurants] = useState([])
   const { id } = useParams()
 
   const getTeamById = () => {
@@ -46,11 +48,38 @@ export default function Team() {
       })
   }
 
+  const getRestaurant = () => {
+    fetch('/api/v1/restaurants')
+      .then(res => res.json())
+      .then(data => {
+        setTeamRestaurants(data)
+      })
+  }
 
   useEffect(() => {
     getTeamById()
     allUsers()
+    getRestaurant()
   }, [setTeam, setTeamUsers])
+
+
+function handleOnDragEnd(result) {
+  if(!result.destination) return;
+console.log(result)
+const state = {
+  restaurants:Array.from(teamRestaurants),
+  fight:Array.from(fightRestaurants)
+}
+const source = result.source.droppableId
+const destination = result.destination.droppableId
+const [reorderedItem] = state[source].splice(result.source.index, 1)
+  state[destination].splice(result.destination.index, 0, reorderedItem)
+
+  setTeamRestaurants(state.restaurants)
+  setFightRestaurants(state.fight)
+}
+
+
 
   return (
     <div>
@@ -60,21 +89,75 @@ export default function Team() {
           <h1 className="col-12 teamName">{team.name}</h1>
           <Button className="col-3 mx-auto joinButton" variant="dark" onClick={handleClick}>Join Team</Button>
         </Row>
-
         <Card className="col-6 teamMemberCard">
           <Card.Title className="teamMemberCardTitle pb-0">Team Members</Card.Title>
           <Card.Body>
             <div className="memberList">
               {teamUsers.map(user => {
                 return (<div className="memberListItem p-3">
-                    {user.username}
+                  {user.username}
                 </div>
                 )
               })}
             </div>
           </Card.Body>
         </Card>
+      </Row>
 
+
+
+
+
+      <Row>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Card className="col-4 mx-auto ">
+          <Card.Title>Restaurants</Card.Title>
+            <Droppable droppableId="restaurants">
+              {(provided) => (
+                <Card.Body {...provided.droppableProps} ref={provided.innerRef}>
+                  {teamRestaurants.map((restaurant, index) => {
+                    return (
+                      <Draggable key={restaurant.id} draggableId={restaurant.id.toString()} index={index}>
+                        {(provided) => (
+                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        <ul>
+                        {restaurant.name}
+                        </ul>
+                      </div>
+                        )}
+                      </Draggable>
+                    )
+                  })}
+                </Card.Body>
+              )}
+            </Droppable>
+        </Card>
+
+
+
+        <Card className="col-6">
+          <Card.Title>Fights</Card.Title>
+            <Droppable droppableId="fight">
+              {(provided) => (
+                <Card.Body {...provided.droppableProps} ref={provided.innerRef}>
+                  {fightRestaurants.map((restaurant, index) => {
+                    return (
+                      <Draggable key={restaurant.id} draggableId={restaurant.id.toString()} index={index}>
+                        {(provided) => (
+                      <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        <ul>
+                        {restaurant.name}
+                        </ul>
+                      </div>
+                        )}
+                      </Draggable>
+                    )
+                  })}
+                </Card.Body>
+              )}
+            </Droppable>
+        </Card>
+          </DragDropContext>
       </Row>
       <Footer />
     </div>
