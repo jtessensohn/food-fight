881c9b67916251d6492d9166306d26495c7e6f10
Tyler Hobbs <tylerhobbs94@hotmail.com>
2021-04-28 12:16:39 -0400

    deleted navigation from team page

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -59,7 +59,6 @@ export default function Team() {
 
   return (
     <div>
-      <Navigation />
       <Row className="mt-3">
         <Row className="col-6">
           <h1 className="col-12 p-0 my-auto teamNameTeamPage">{teamData.name}</h1>
@@ -79,11 +78,8 @@ export default function Team() {
           </Card.Body>
         </Card>
       </Row>
-
       <Fight />
-      <Footer />
     </div>
   )
 }
 
-}
